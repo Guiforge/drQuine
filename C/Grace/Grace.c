1af40c92310f2e3840ea6ad38be7f19092aa128d
gpouyat <gpouyat@vejja.fr>
2018-09-03 18:54:19 +0200

    Grace: Close fd

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 /*
  Beautiful comment
 */
 
 #define OPEN open("./Grace_kid.c", O_WRONLY | O_TRUNC | O_CREAT)
-#define CODE "#include <stdio.h>%c#include <fcntl.h>%c%c/*%c Beautiful comment%c*/%c%c#define OPEN open(%c./Grace_kid.c%c, O_WRONLY | O_TRUNC | O_CREAT)%c#define CODE %c%s%c%c#define FT(x)int main(){ dprintf(OPEN, CODE, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 10, 10); }%cFT()%c"
-#define FT(x)int main(){ dprintf(OPEN, CODE, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 10, 10); }
+#define CODE "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c/*%c Beautiful comment%c*/%c%c#define OPEN open(%c./Grace_kid.c%c, O_WRONLY | O_TRUNC | O_CREAT)%c#define CODE %c%s%c%c#define FT(x)int main(){ int fd=OPEN; dprintf(fd, CODE, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 10, 10);close(fd); }%cFT()%c"
+#define FT(x)int main(){ int fd=OPEN; dprintf(fd, CODE, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 10, 10);close(fd); }
 FT()
