d74f93011371c531a568b117b03726d94eb786cb
gpouyat <gpouyat@vejja.fr>
2018-09-03 18:49:13 +0200

    Colleen: change comment + makefile remove useless

--- C/Colleen/Colleen.c
+++ C/Colleen/Colleen.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* tata */
+/*
+ tata
+*/
 
 char return_n()
 {
@@ -9,7 +11,9 @@ char return_n()
 
 int main (void)
 {
-	/* toto */
-	char *data = "#include <stdio.h>%c%1$c/* tata */%1$c%1$cchar return_n()%1$c{%1$c	return(10);%1$c}%1$c%1$cint main (void)%1$c{%1$c	/* toto */%1$c	char *data = %c%s%c;%1$c	printf(data, return_n(), 34, data, 34);%1$c}%1$c";
+	/*
+	 toto
+	*/
+	char *data = "#include <stdio.h>%c%1$c/*%1$c tata%1$c*/%1$c%1$cchar return_n()%1$c{%1$c	return(10);%1$c}%1$c%1$cint main (void)%1$c{%1$c	/*%1$c	 toto%1$c	*/%1$c	char *data = %c%s%c;%1$c	printf(data, return_n(), 34, data, 34);%1$c}%1$c";
 	printf(data, return_n(), 34, data, 34);
 }
