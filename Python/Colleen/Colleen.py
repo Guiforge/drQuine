e3197da03357d82c57515af0a37056800da2971f
Guillaume Pouyat <guiforge@gmail.com>
2018-11-14 11:57:15 +0100

    Change shebang python

--- Python/Colleen/Colleen.py
+++ Python/Colleen/Colleen.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 
 # Comment beautifull
 def break_line():
@@ -6,7 +6,7 @@ def break_line():
 
 def main():
     #Beautifull comment
-    data="#!/usr/local/bin/python3{bl}{bl}# Comment beautifull{bl}def break_line():{bl}    return chr(10){bl}{bl}def main():{bl}    #Beautifull comment{bl}    data={qt}{data}{qt}{bl}    print(data.format(bl=break_line(), qt=chr(34), data=data)){bl}{bl}main()"
+    data="#!/usr/bin/python3{bl}{bl}# Comment beautifull{bl}def break_line():{bl}    return chr(10){bl}{bl}def main():{bl}    #Beautifull comment{bl}    data={qt}{data}{qt}{bl}    print(data.format(bl=break_line(), qt=chr(34), data=data)){bl}{bl}main()"
     print(data.format(bl=break_line(), qt=chr(34), data=data))
 
 main()
--- Python/Grace/Grace.py
+++ Python/Grace/Grace.py
@@ -1,8 +1,8 @@
-#!/usr/local/bin/python3
+#!/usr/bin/python3
 import sys
 
 # Comment
-data="#!/usr/local/bin/python3{brk}import sys{brk}{brk}# Comment{brk}data={qt}{data}{qt}{brk}brk=chr(10){brk}try:{brk}	text_file = open({qt}Grace_kid.py{qt}, {qt}w{qt}){brk}	text_file.write(data.format(brk=brk, qt=chr(34), data=data, fbr=chr(123), lbr=chr(125))){brk}	text_file.close(){brk}except IOError as e:{brk}	print({qt}I/O error({fbr}0{lbr}): {fbr}1{lbr}{qt}.format(e.errno, e.strerror)){brk}except ValueError:{brk}	print({qt}Could not convert data to an integer.{qt}){brk}except:{brk}	print({qt}Unexpected error:{qt}, sys.exc_info()[0]){brk}	raise"
+data="#!/usr/bin/python3{brk}import sys{brk}{brk}# Comment{brk}data={qt}{data}{qt}{brk}brk=chr(10){brk}try:{brk}	text_file = open({qt}Grace_kid.py{qt}, {qt}w{qt}){brk}	text_file.write(data.format(brk=brk, qt=chr(34), data=data, fbr=chr(123), lbr=chr(125))){brk}	text_file.close(){brk}except IOError as e:{brk}	print({qt}I/O error({fbr}0{lbr}): {fbr}1{lbr}{qt}.format(e.errno, e.strerror)){brk}except ValueError:{brk}	print({qt}Could not convert data to an integer.{qt}){brk}except:{brk}	print({qt}Unexpected error:{qt}, sys.exc_info()[0]){brk}	raise"
 brk=chr(10)
 try:
 	text_file = open("Grace_kid.py", "w")
