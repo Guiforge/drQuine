19f93704a4c81b483b45a0484c539e8c73c613c0
Guillaume Pouyat <guiforge@gmail.com>
2018-11-14 12:11:28 +0100

    Mini fix + add auteur file

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -3,10 +3,10 @@
 #include <unistd.h>
 
 /*
- Beautiful comment
+   Beautiful comment
 */
 
 #define OPEN open("./Grace_kid.c", O_WRONLY | O_TRUNC | O_CREAT)
-#define CODE "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c/*%c Beautiful comment%c*/%c%c#define OPEN open(%c./Grace_kid.c%c, O_WRONLY | O_TRUNC | O_CREAT)%c#define CODE %c%s%c%c#define FT(x)int main(){ int fd=OPEN; dprintf(fd, CODE, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 10, 10);close(fd); }%cFT()%c"
+#define CODE "#include <stdio.h>%c#include <fcntl.h>%c#include <unistd.h>%c%c/*%c   Beautiful comment%c*/%c%c#define OPEN open(%c./Grace_kid.c%c, O_WRONLY | O_TRUNC | O_CREAT)%c#define CODE %c%s%c%c#define FT(x)int main(){ int fd=OPEN; dprintf(fd, CODE, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 10, 10);close(fd); }%cFT()%c"
 #define FT(x)int main(){ int fd=OPEN; dprintf(fd, CODE, 10, 10, 10, 10, 10, 10, 10, 10, 34, 34, 10, 34, CODE, 34, 10, 10, 10);close(fd); }
 FT()
